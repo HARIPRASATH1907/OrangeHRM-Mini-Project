48fcb80de07fadd8897e2cc94ddf4ff5cf4d330e
HARIPRASATH1907 <142893798+HARIPRASATH1907@users.noreply.github.com>
2023-11-08 14:30:59 +0530

    Create testcase1.java
    pull this code

---
+++ myprojectProjectName/src/test/java/myprojectProjectName/testcase1.java
@@ -0,0 +1,28 @@
+void Duplication() {
+		int[] a= {6,1,6,2,6,0,2,1 };
+		int[] c_v=new int[a.length];    //c_v--count_value
+			
+		
+				//System.out.println("The duplicate values are");
+			for(int i=0;i<a.length;i++)
+			{
+				int count=1;
+				for(int j=i+1;j<a.length;j++)
+				{						
+					if(a[i]==a[j]) 
+					{
+						count++;
+						c_v[j]=-1;
+					}
+				}
+				if(c_v[i]!=-1)
+				{
+					c_v[i]=count;
+				
+				System.out.println(a[i]+" --> "+c_v[i]+" count");
+				}
+				if(c_v[i]>=2) {
+					//System.out.println(a[i]);
+				}
+			}
+		}
